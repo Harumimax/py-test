a0e1cf029f8ef6cd9c468b8663240f6dfba187d4
harumimax <harumimax@gmail.com>
2023-04-28 16:20:42 +0300

    calc + 2 task

--- games/cards/utils.py
+++ games/cards/utils.py
@@ -4,13 +4,21 @@ def get_min_card(deck_list):
     if type(deck_list) != list:
         deck_list = deck_list.return_list()
     index_min_card = len(NumsList)
+    result = False
     for current_card in deck_list:
         if index_min_card >= NumsList.index(current_card.num):
             index_min_card = NumsList.index(current_card.num)
             result = current_card
-    if "result" not in locals():
-        result = False
     return result
+
+def get_index_max_card(list_of_card):
+    cards_indexes = []
+    for current_card in list_of_card:
+        cards_indexes.append(NumsList.index(current_card.num))
+    max_card_index = max(cards_indexes)
+    result = False if cards_indexes.count(max_card_index) > 1 else cards_indexes.index(max_card_index)
+    return result
+
 
 def calc(str):
     list_str = list(str)
@@ -41,4 +49,25 @@ def calc(str):
         else:
             pass
     return result
+
+def unique_words(str):
+    str_list = str.split()
+    sings = ["?", ".", ",", "!", "-"]
+    str_list = [current_word[:-1].lower() if current_word[-1] in sings else current_word.lower() for current_word in str_list]
+    print(str_list)
+    result = []
+    for current_word in str_list:
+        if result.count(current_word) == 0:
+            result.append(current_word)
+    return result
+
+print(unique_words("Python is great, isn't it? is Yes- yes blat"))
+
+def filter_capitalized_words(list):
+    result = []
+    for current_str in list:
+        if current_str[0] == current_str[0].upper():
+            result.append(current_str)
+    return result
+
 
